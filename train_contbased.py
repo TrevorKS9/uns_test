9f5e41f31bdd7e3dd36d6324c12f763b666d5278
TrevorKS9 <61237583+TrevorKS9@users.noreply.github.com>
2020-07-02 09:48:00 +0200

    Add files via upload

---
+++ train_contbased.py
@@ -0,0 +1,29 @@
+# Importing packages and Data
+import pandas as pd
+from rake_nltk import Rake
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import CountVectorizer
+import pickle
+# Importing data
+
+movies = pd.read_csv('movies.csv',sep = ',',delimiter=',')
+ratings = pd.read_csv('ratings.csv')
+movies.dropna(inplace=True)
+def datapreprocessing():
+    # spliting the genres
+    movies['keyWords'] = movies['genres'].str.replace('|', ' ')
+  
+    return movies
+def content_model():
+    # initializing the empty list of recommended movies
+    recommended_movies = []
+    data = datapreprocessing()
+    # instantiating and generating the count matrix
+    count_vec = CountVectorizer()
+    count_matrix = count_vec.fit_transform(data['keyWords'])
+    indices = pd.Series(data['title'])
+    return pickle.dump(cosine_similarity(count_matrix, count_matrix,dtype='uint8'), open(save_path,'wb'))
+content_model()
+    
+    
